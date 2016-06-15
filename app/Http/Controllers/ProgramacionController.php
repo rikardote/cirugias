70b3ac763cff029f5d56233a76dac0b2ee7d78a9
rikardote <ricarmenta@gmail.com>
2016-06-15 14:26:04 -0700

    add surgurys table

--- app/Http/Controllers/ProgramacionController.php
+++ app/Http/Controllers/ProgramacionController.php
@@ -6,13 +6,14 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use Carbon\Carbon;
+use App\Surgery;
 
 class ProgramacionController extends Controller
 {
     public function index()
     {
     	$today = Carbon::today();
-    	$cirugias = Programa
-    	return view('programar_cirugia.index')->with('today', $today);
+    	$cirugias = Surgery::all();
+    	return view('programar_cirugia.index')->with('today', $today)->with('cirugias', $cirugia);
     }
 }
