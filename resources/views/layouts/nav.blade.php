54b49323d0b841fc04d8a33fc4e3902d40053bea
vagrant <vagrant@homestead>
2017-02-01 05:23:11 +0000

    cirguias casi lista

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -79,13 +79,13 @@
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                               
+                               test <span class="caret"></span>
                             </a>
 
                             <ul class="dropdown-menu" role="menu">
                                 
                                    
-                                  
+                                 
                                     <li>
                                         <a href="{{ url('/dianohabil') }}"><i class="fa fa-btn fa fa-cog"></i>Dias no habiles</a>
                                     </li>
@@ -93,8 +93,7 @@
                                         <a href="{{ url('/registrar') }}"><i class="fa fa-btn fa fa-cog"></i>Administrar Usuarios</a>
                                     </li>
                                     
-                                  
-                              
+                                 
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Salir</a></li>
                             </ul>
                         </li>
