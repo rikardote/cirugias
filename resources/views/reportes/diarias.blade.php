a0f03d95a8efea2245dd0d7f5f1ca8db68e12a44
rikardote <ricarmenta@gmail.com>
2017-02-01 19:30:02 +0000

    fix sala en reporte diario

--- resources/views/reportes/diarias.blade.php
+++ resources/views/reportes/diarias.blade.php
@@ -19,7 +19,7 @@
 			
 			<tr>
 				<td class='font-small' valign=top>{{$cirugia->horario}}</td>
-				<td class='font-small' valign=top>{{ ($cirugia->sala==4) ? 'Ext':$cirugia->sala }}</td>
+				<td class='font-small' valign=top>{{ ($cirugia->sala==0) ? 'Ext':$cirugia->sala }}</td>
 				<td class='font-small'>{{$cirugia->paciente->fullname}} <br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{$cirugia->paciente->rfc}} /{{$cirugia->paciente->tipo->code}} ({{$cirugia->ubicacion}})<br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{$cirugia->medico->fullname}}</td>
 				<td class='font-small'>{{$cirugia->cirugia->name}} <br><br> {{$cirugia->anestesiologo->fullname}} </td>
 			</tr>
